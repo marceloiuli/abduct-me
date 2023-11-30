3938220eb3a0682114de9e003d29c7ffe3238e9c
marceloiuli <marceloiuli26@gmail.com>
2023-11-29 18:53:04 -0800

    Made changes to design in bootstrap

--- src/components/AbductionForm/AbductionForm.jsx
+++ src/components/AbductionForm/AbductionForm.jsx
@@ -4,6 +4,8 @@ import * as abductionsAPI from "../../utilities/abductions-api"
 import "./AbductionForm.css";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function AbductionForm({abduction}) {
     const [abductions, setAbductions] = useState({name : '', phone: '', email: '', date: '', ufoChoice: '', abType: '', specialRequest: ''})
@@ -53,25 +55,47 @@ export default function AbductionForm({abduction}) {
     return (
         <body class="formBody">
             <Form class="formInfo" autoComplete="off" onSubmit={handleSubmit}>
+                <legend>Your Contact Information:</legend>
+                <br />
+                <Row className="mb-3">
+                    <br></br>
+                    <Form.Group as={Col}>
+                        <Form.Label htmlFor="name">Your Name</Form.Label>
+                        <Form.Control onChange={handleChange} type="text" id="name" name="name" placeholder="First Name & Last Name" defaultValue={abductions.name} />
+                    </Form.Group>
+                    <br></br>
+                    <Form.Group as={Col}>
+                        <Form.Label htmlFor="phone">Phone Number</Form.Label>
+                        <Form.Control onChange={handleChange} type="tel" id="phone" name="phone" placeholder="Dont forget your area code!" defaultValue={abductions.phone} />
+                    </Form.Group>
+                    <br></br>
+                    <Form.Group as={Col}>
+                        <Form.Label htmlFor="email">Email</Form.Label>
+                        <br></br>
+                        <Form.Control onChange={handleChange} type="email" id="email" name="email" placeholder="example123@email.com" defaultValue={abductions.email} />
+                    </Form.Group>
+                </Row>
+
+                <legend>About Your Abduction:</legend>
+                <br />
+                <Row className="mb-3">
+                    <Form.Group as={Col}>
+
+                        <Form.Label htmlFor="date">Requested Date</Form.Label>
+                        <Form.Control onChange={handleChange} type="date" id="date" name="date" defaultValue={abductions.date ? abductions.date.split('T')[0] : ''} />
+
+                    </Form.Group>
+
+                    <Form.Group as={Col}>
+
+                        <Form.Label htmlFor="qty">Would you like to bring friends?</Form.Label>
+                        <Form.Control onChange={handleChange} type="number" id="qty" name="qty" min="0" max="5" defaultValue="0" placeholder="5 humans maximum" />
+
+                    </Form.Group>
+                </Row>
+
                 <Form.Group>
-                    <legend>Your Contact Information:</legend>
-                    <Form.Label htmlFor="name">Your Name</Form.Label>
-                    <Form.Control onChange={handleChange} type="text" id="name" name="name" placeholder="First Name & Last Name" defaultValue={abductions.name} />
-                    <label htmlFor="phone">Phone Number</label>
-                    <input onChange={handleChange} type="tel" id="phone" name="phone" placeholder="Dont forget your area code!" defaultValue={abductions.phone} />
-                    <label htmlFor="email">Email</label>
-                    <input onChange={handleChange} type="email" id="email" name="email" placeholder="example123@email.com" defaultValue={abductions.email} />
-                </Form.Group>
-                <Form.Group>
-                    <legend>About Your Abduction:</legend>
-                    <label htmlFor="date">Requested Date</label>
-                    <input onChange={handleChange} type="date" id="date" name="date" defaultValue={abductions.date ? abductions.date.split('T')[0] : ''} />
-                </Form.Group>
-                <Form.Group>
-                    <label htmlFor="qty">Would you like to bring friends?</label>
-                    <input onChange={handleChange} type="number" id="qty" name="qty" min="0" max="5" defaultValue="0" placeholder="5 humans maximum" />
-                </Form.Group>
-                <Form.Group>
+
                     <legend>Please choose your preferred ufo type</legend>
                     <Form.Label>
                         <Form.Check onChange={handleChange} type="radio" name="ufoChoice" value="classic" checked={abductions.ufoChoice === 'classic'} />Classic Oval UFO
@@ -89,8 +113,11 @@ export default function AbductionForm({abduction}) {
                         <Form.Check onChange={handleChange} type="radio" name="ufoChoice" value="borg" checked={abductions.ufoChoice === 'borg'} />Borg-style Cube
                     </Form.Label>
                     <br />
+
                 </Form.Group>
+
                 <Form.Group>
+
                     <Form.Label htmlFor="abType">What type of abduction experience do you want?</Form.Label>
                     <Form.Select size="lg" onChange={handleChange} name="abType" id="abtype">
                         <option defaultValue>Choose Wisely...</option>
@@ -99,15 +126,20 @@ export default function AbductionForm({abduction}) {
                         <option value="dance" selected={abductions.abType === 'dance'}>Alien dance lessons</option>
                         <option value="whale" selected={abductions.abType === 'whale'}>Whale watching</option>
                     </Form.Select>
+
                 </Form.Group>
+        
                 <Form.Group>
-                    <label htmlFor="specialRequests">Special Requests:</label>
+
+                    <Form.Label htmlFor="specialRequests">Special Requests:</Form.Label>
                     <textarea onChange={handleChange} name="specialRequests" id="specialRequests" cols="30" rows="10" placeholder="Tell us everything..." maxLength="180" defaultValue={abductions.specialRequest}></textarea>
                     &nbsp; &nbsp;
                     <Button variant="dark" type="submit">SUBMIT</Button>
                     &nbsp; &nbsp;
                     <Button variant="dark" onClick={onDelete} >{abductions._id ? "DELETE" : "CANCEL"}</Button>
+
                 </Form.Group>
+
             </Form>
         </body>
     )
